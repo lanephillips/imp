6efbf7eb13b124bdf1b4460e413082ac2f4da5db
Lane Phillips <lane@milkllc.com>
2015-02-15 10:08:29 -0600

    refactor DB stuff

--- imp.go
+++ imp.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gorilla/mux"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
 	"github.com/unrolled/render"
 )
 
@@ -20,6 +19,18 @@ func main() {
 		// TODO: die
 	}
 	// fmt.Println("loaded config", cfg)
+
+	// set up database connection
+	db, err := sql.Open("mysql", cfg.Database.User + ":" + cfg.Database.Password + "@/" + cfg.Database.Database)
+	if err != nil {
+	    panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+	    panic(err.Error()) // proper error handling instead of panic in your app
+	}
 
     // Heroku uses env var to specify port
     port := os.Getenv("PORT")
@@ -40,7 +51,17 @@ func main() {
     r.HandleFunc("/", HomeHandler)
 
     api := r.PathPrefix("/api").Subrouter()
-    api.HandleFunc("/user/{id}", UsersHandler)
+
+    api.HandleFunc("/user/{id}", func (rw http.ResponseWriter, r *http.Request) {
+	    id := mux.Vars(r)["id"]
+
+	    users := QueryUsers(db)
+
+		rend := render.New()
+	    rend.JSON(rw, http.StatusOK, users)
+
+	    fmt.Fprintln(rw, "showing user", id)
+	})
 
 	http.ListenAndServeTLS(cfg.Server.Host + ":" + port, cfg.Server.Certificate, cfg.Server.Key, r)
 }
@@ -48,36 +69,4 @@ func main() {
 func HomeHandler(rw http.ResponseWriter, r *http.Request) {
 	rend := render.New()
 	rend.HTML(rw, http.StatusOK, "login", nil)
-}
-
-func UsersHandler(rw http.ResponseWriter, r *http.Request) {
-	dsn := fmt.Sprint(cfg.Database.User, ":", cfg.Database.Password, "@/", cfg.Database.Database)
-	db, err := sql.Open("mysql", dsn)
-	// fmt.Println("%v %v", db, err)
-
-    id := mux.Vars(r)["id"]
-
-	rows, err := db.Query("SELECT `UserId`, `Handle`, `Status`, `Biography`, `JoinedDate` FROM `User` WHERE 1")	// TODO: user id
-	if err != nil {
-	    log.Fatal(err)
-	    // TODO: 500
-	}
-
-	var users []*User
-	for rows.Next() {
-	    var user User
-	    if err := rows.Scan(&user.UserId, &user.Handle, &user.Status, &user.Biography, &user.JoinedDate); err != nil {
-	        log.Fatal(err)
-	    }
-	    users = append(users, &user)
-	}
-	if err := rows.Err(); err != nil {
-	    log.Fatal(err)
-	    // TODO: 500
-	}
-
-	rend := render.New()
-    rend.JSON(rw, http.StatusOK, users)
-
-    fmt.Fprintln(rw, "showing user", id)
 }
--- models.go
+++ models.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/go-sql-driver/mysql"
+import (
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+	"log"
+)
 
 type User struct {
 	UserId int64
@@ -8,4 +12,31 @@ type User struct {
 	Status string
 	Biography string
 	JoinedDate mysql.NullTime
+}
+
+func (u *User) Fetch(db *sql.DB, userId int64) {
+	// TODO: 
+}
+
+// TODO: filters
+func QueryUsers(db *sql.DB) (users []*User) {
+	rows, err := db.Query("SELECT `UserId`, `Handle`, `Status`, `Biography`, `JoinedDate` FROM `User` WHERE 1")	// TODO: user id
+	if err != nil {
+	    log.Fatal(err)
+	    // TODO: 500
+	}
+
+	users = make([]*User, 0)
+	for rows.Next() {
+	    var user User
+	    if err := rows.Scan(&user.UserId, &user.Handle, &user.Status, &user.Biography, &user.JoinedDate); err != nil {
+	        log.Fatal(err)
+	    }
+	    users = append(users, &user)
+	}
+	if err := rows.Err(); err != nil {
+	    log.Fatal(err)
+	    // TODO: 500
+	}
+	return
 }
