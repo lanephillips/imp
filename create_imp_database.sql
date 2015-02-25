9e5a462372b7bdad966f48dda8571b512c57b7a2
Lane Phillips <lane@milkllc.com>
2015-02-25 13:05:09 -0600

    new db tables for rate limiting

--- create_imp_database.sql
+++ create_imp_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 20, 2015 at 09:52 PM
+-- Generation Time: Feb 25, 2015 at 07:03 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.14
 
@@ -36,12 +36,38 @@ CREATE TABLE IF NOT EXISTS `Guest` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `HandleLimit`
+--
+
+CREATE TABLE IF NOT EXISTS `HandleLimit` (
+  `Handle` varchar(16) NOT NULL,
+  `LoginAttemptCount` int(11) NOT NULL,
+  `LastAttemptDate` datetime NOT NULL,
+  `NextLoginDelay` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Host`
 --
 
 CREATE TABLE IF NOT EXISTS `Host` (
 `HostId` int(11) NOT NULL,
   `Name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `IPLimit`
+--
+
+CREATE TABLE IF NOT EXISTS `IPLimit` (
+  `IP` varchar(45) NOT NULL,
+  `LastLoginAttemptDate` datetime DEFAULT NULL,
+  `UsersAllowedCount` tinyint(4) NOT NULL,
+  `CountResetDate` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -56,9 +82,13 @@ CREATE TABLE IF NOT EXISTS `User` (
   `Status` varchar(140) NOT NULL DEFAULT '',
   `Biography` varchar(140) NOT NULL DEFAULT '',
   `Email` varchar(254) NOT NULL,
+  `IsValidEmail` tinyint(1) NOT NULL DEFAULT '0',
+  `EmailValidationToken` varchar(50) DEFAULT NULL,
+  `EmailValidationDate` date DEFAULT NULL,
   `PasswordHash` varchar(60) NOT NULL,
-  `JoinedDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `JoinedDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `IsDisabled` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -72,6 +102,19 @@ CREATE TABLE IF NOT EXISTS `UserHost` (
   `Token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `UserToken`
+--
+
+CREATE TABLE IF NOT EXISTS `UserToken` (
+  `Token` varchar(50) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `LoginTime` date NOT NULL,
+  `LastSeenTime` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -83,16 +126,34 @@ ALTER TABLE `Guest`
  ADD PRIMARY KEY (`GuestId`);
 
 --
+-- Indexes for table `HandleLimit`
+--
+ALTER TABLE `HandleLimit`
+ ADD PRIMARY KEY (`Handle`);
+
+--
 -- Indexes for table `Host`
 --
 ALTER TABLE `Host`
  ADD PRIMARY KEY (`HostId`);
 
 --
+-- Indexes for table `IPLimit`
+--
+ALTER TABLE `IPLimit`
+ ADD PRIMARY KEY (`IP`);
+
+--
 -- Indexes for table `User`
 --
 ALTER TABLE `User`
- ADD PRIMARY KEY (`UserId`);
+ ADD PRIMARY KEY (`UserId`), ADD UNIQUE KEY `Handle` (`Handle`), ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `UserToken`
+--
+ALTER TABLE `UserToken`
+ ADD PRIMARY KEY (`Token`);
 
 --
 -- AUTO_INCREMENT for dumped tables
